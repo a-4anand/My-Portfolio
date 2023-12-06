c2d19b345c2a4bbd94c5d454c5d98190d3df547a
Anand kumar dubey <142786893+a-4anand@users.noreply.github.com>
2023-12-06 19:36:38 +0530

    Add files via upload

---
+++ programs/source.js
@@ -0,0 +1,9 @@
+document.getElementById("projectsButton").onclick = function() {
+    window.location.href="programs/project.html"
+  };
+document.getElementById("experienceButton").onclick = function() {
+    window.location.href = "programs/experience.html"; 
+  };
+
+
+  
